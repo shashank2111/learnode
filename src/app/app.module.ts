ee335880590d4945167c54c8c61106a6e9b43c81
Shashank Adkeshwar <shashankadkeshwar1@gmail.com>
2020-08-12 16:22:32 +0530

    routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,28 @@ import { MatButtonModule } from "@angular/material/button";
 import { FormsModule } from "@angular/forms";
 
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+
 import { DishService } from "./services/dish.service";
 
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +44,9 @@ import { DishService } from "./services/dish.service";
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
+    
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
